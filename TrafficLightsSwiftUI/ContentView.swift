75b5d872b4bcc037243b9203234ed19cd1d59f47
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-12 14:34:36 +0500

    Ready!

--- TrafficLightsSwiftUI/ContentView.swift
+++ TrafficLightsSwiftUI/ContentView.swift
@@ -17,20 +17,26 @@ struct ContentView: View {
 
             VStack(spacing: 32) {
                 createCircleWith(diameter: 100, color: .red)
-                    .opacity(currentPosition == 1 ? 1.0 : 0.5)
+                    .opacity(currentPosition == 1 ? 1.0 : 0.3)
+                    .shadow(color: .red,
+                            radius: currentPosition == 1 ? 7 : 0)
                 createCircleWith(diameter: 100, color: .yellow)
-                    .opacity(currentPosition == 2 ? 1.0 : 0.5)
+                    .opacity(currentPosition == 2 ? 1.0 : 0.3)
+                    .shadow(color: .yellow,
+                            radius: currentPosition == 2 ? 7 : 0)
                 createCircleWith(diameter: 100, color: .green)
-                    .opacity(currentPosition == 3 ? 1.0 : 0.5)
+                    .opacity(currentPosition == 3 ? 1.0 : 0.3)
+                    .shadow(color: .green,
+                            radius: currentPosition == 3 ? 7 : 0)
                 Spacer()
-                Button(action: switchSemaphore) {
+                Button(action: switchLight) {
                     Text(buttonTitle)
                         .foregroundColor(.white)
-                        .font(.title).bold()
+                        .font(.title)
+                        .bold()
                 }
-                .tint(.blue)
-                .shadow(color: .gray, radius: 5)
                 .buttonStyle(.borderedProminent)
+                .tint(.blue)
             }
             .padding(16)
         }
@@ -40,15 +46,15 @@ struct ContentView: View {
 extension ContentView {
     private func createCircleWith(diameter: CGFloat, color: Color) -> some View {
         Circle()
+            .padding(-4)
             .frame(width: diameter, height: diameter)
             .foregroundColor(color)
-            .padding(-3)
-            .shadow(color: color, radius: 7)
-            .overlay(Circle().trim(from: 0.5, to: 1)
-                .stroke(Color.black.opacity(0.8), lineWidth: 6))
+            .overlay(Circle().trim(from: 0.53, to: 0.97)
+                .stroke(Color.black.opacity(0.8), lineWidth: 8))
+            
     }
 
-    func switchSemaphore() {
+    func switchLight() {
         if buttonTitle == "START" { buttonTitle = "NEXT" }
         currentPosition < 3 ? (currentPosition += 1) : (currentPosition = 1)
     }
