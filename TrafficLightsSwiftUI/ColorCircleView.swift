1fc8fe2594d326fcfb60ea83ece167f78a175030
Aleksandr Kretov <kretov.1988@gmail.com>
2022-05-13 23:29:28 +0500

    Latest release, shadow, animations etc...

--- TrafficLightsSwiftUI/ColorCircleView.swift
+++ TrafficLightsSwiftUI/ColorCircleView.swift
@@ -20,7 +20,6 @@ struct ColorCircleView: View {
             .foregroundColor(color)
             .shadow(color: color, radius: opacity == 1 ? 12 : 0)
             .opacity(opacity)
-            .shadow(color: color, radius: opacity == 1 ? 12 : 0)
             .overlay(Circle()
                 .trim(from: 0.53, to: 0.97)
                 .stroke(Color.brown, lineWidth: 8))
